a56541c8beffbc810637704f978125aa01a861c4
Akram A <akram.f11@gmail.com>
2018-12-23 19:53:42 +0100

    -added more Enemies. -added comments

--- js/app.js
+++ js/app.js
@@ -36,6 +36,12 @@ class Player {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
   }
 
+//check for collision:
+// if the player Y locaion equals to a Y locaion of any of the enimies and
+// if the x locaion of player is in the range of an enemy's width X location
+// the width was calculated to suit the gameplay the best (+60 from the front and -50 from the rare)
+// the second if statments check if the player has reached the water (won).
+// in both cases -collision or winning- the game resets itself, setting the player back to the initial location.
   update(){
     for(let enemy of allEnemies){
       if (this.y === enemy.y && enemy.x - 50 < this.x && enemy.x + 60 > this.x) {
@@ -47,6 +53,10 @@ class Player {
     }
   }
 
+//handle arrow keys presses
+//in order to move the player a block vertically on each press, x valuve needs to be changed by the value 101
+//in order to move the player a block horizontally on each press, y valuve needs to be changed by the value 83
+//an if statment on each key to prevents the player to go off the canvas.
   handleInput(key) {
     switch (key) {
       case 'left':
@@ -72,14 +82,15 @@ class Player {
     }
   }
 
+//set the location to the initial axis
   reset(){
     this.x = 202;
     this.y = 387;
   }
 }
 
-
-let allEnemies = [new Enemy(55, 2.5), new Enemy(55 + 83, 2.7), new Enemy(55 + 83 * 2, 0.2)];
+//adding enemies to allEnemies array, setting the Y to one of the three columns, and the speed to a different value.
+let allEnemies = [new Enemy(55, 2.5),new Enemy(55, 2), new Enemy(55 + 83, 2.7), new Enemy(55 + 83 * 2, 0.2), new Enemy(55 + 83 * 2, 1.2)];
 const player = new Player();
 
 // This listens for key presses and sends the keys to your
