d7974cd2af45b8cbc5dcc027dd6f27a4dfe1f75e
Akram A <akram.f11@gmail.com>
2018-12-23 18:18:25 +0100

    Created "player" Class that has the props and methods >  render, update and reset

--- js/app.js
+++ js/app.js
@@ -33,6 +33,7 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+
 class Player {
   //constructor
   constructor() {
